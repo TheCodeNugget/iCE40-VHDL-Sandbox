f6c1ed45b52822e9272506981ab33f592c3e2bf9
TheCodeNugget <95768094+TheCodeNugget@users.noreply.github.com>
2024-05-16 15:13:30 +0300

    refactor: i2c rx vhdl-2008 refactor

--- I2C_Master_Controller/i2c_rx.vhd
+++ I2C_Master_Controller/i2c_rx.vhd
@@ -20,6 +20,7 @@ end i2c_rx;
 
 architecture rtl of i2c_rx is
 
+    -- RX Architecture States
     constant c_rx_idle : std_logic_vector(3 downto 0) := b"0000";
     constant c_rx_bit0 : std_logic_vector(3 downto 0) := b"0001";
     constant c_rx_bit1 : std_logic_vector(3 downto 0) := b"0010";
@@ -40,35 +41,37 @@ begin
     -- RX FSM
     -----------------------------------------------------------
 
-    rst : process (rising_edge(i_clk), rising_edge(i_rst)) is
+    rst : process (i_clk, i_rst) is
     begin
         if (i_rst) then
             r_rx_currState <= c_rx_idle;
-        else
+        elsif (rising_edge(i_clk)) then
             r_rx_currState <= r_rx_nextState;
         end if;
     end process;
 
     rx_fsm : process(all)
     begin
-        r_rx_nextState <= r_rx_currState;
-        if (r_rx_currState = c_rx_idle) then
-            if (i_en) then
-                r_rx_nextState <= c_rx_bit7;
+        if (rising_edge(i_clk)) then
+            r_rx_nextState <= r_rx_currState;
+            if (r_rx_currState = c_rx_idle) then
+                if (i_en) then
+                    r_rx_nextState <= c_rx_bit7;
+                end if;
+            elsif (i_scl_falling_edge) then
+                with r_rx_currState select
+                    r_rx_nextState <=
+                        c_rx_bit6 when c_rx_bit7,
+                        c_rx_bit5 when c_rx_bit6,
+                        c_rx_bit4 when c_rx_bit5,
+                        c_rx_bit3 when c_rx_bit4,
+                        c_rx_bit2 when c_rx_bit3,
+                        c_rx_bit1 when c_rx_bit2,
+                        c_rx_bit0 when c_rx_bit1,
+                        c_rx_wack when c_rx_bit0,
+                        c_rx_idle when c_rx_wack,
+                        c_rx_idle when others;
             end if;
-        elsif (i_scl_falling_edge) then
-            with r_rx_currState select
-                r_rx_nextState <=
-                    c_rx_bit6 when c_rx_bit7,
-                    c_rx_bit5 when c_rx_bit6,
-                    c_rx_bit4 when c_rx_bit5,
-                    c_rx_bit3 when c_rx_bit4,
-                    c_rx_bit2 when c_rx_bit3,
-                    c_rx_bit1 when c_rx_bit2,
-                    c_rx_bit0 when c_rx_bit1,
-                    c_rx_wack when c_rx_bit0,
-                    c_rx_idle when c_rx_wack,
-                    c_rx_idle when others;
         end if;
     end process;
 
@@ -78,17 +81,19 @@ begin
 
     rx_data: process (all) is
     begin
-        if (i_bus_scl) then
-            case r_rx_currState is
-                when c_rx_bit7 => o_data(7) <= i_bus_sda;
-                when c_rx_bit6 => o_data(6) <= i_bus_sda;
-                when c_rx_bit5 => o_data(5) <= i_bus_sda;
-                when c_rx_bit4 => o_data(4) <= i_bus_sda;
-                when c_rx_bit3 => o_data(3) <= i_bus_sda;
-                when c_rx_bit2 => o_data(2) <= i_bus_sda;
-                when c_rx_bit1 => o_data(1) <= i_bus_sda;
-                when c_rx_bit0 => o_data(0) <= i_bus_sda;
-            end case;
+        if (rising_edge(i_clk)) then
+            if (i_bus_scl) then
+                case r_rx_currState is
+                    when c_rx_bit7 => o_data(7) <= i_bus_sda;
+                    when c_rx_bit6 => o_data(6) <= i_bus_sda;
+                    when c_rx_bit5 => o_data(5) <= i_bus_sda;
+                    when c_rx_bit4 => o_data(4) <= i_bus_sda;
+                    when c_rx_bit3 => o_data(3) <= i_bus_sda;
+                    when c_rx_bit2 => o_data(2) <= i_bus_sda;
+                    when c_rx_bit1 => o_data(1) <= i_bus_sda;
+                    when c_rx_bit0 => o_data(0) <= i_bus_sda;
+                end case;
+            end if;
         end if;
     end process;
 
@@ -96,11 +101,11 @@ begin
     -- I2C SDA Driver enable for sending ACK to Transmitter
     -----------------------------------------------------------
 
-    sda_en: process (rising_edge(i_clk), rising_edge(i_rst)) is
+    sda_en: process (i_clk, i_rst) is
     begin
         if (i_rst) then
             o_sda_en <= '1';
-        else
+        elsif (rising_edge(i_clk)) then
             o_sda_en <= '1' when r_rx_currState = c_rx_wack else '0';
         end if;
     end process;
@@ -109,11 +114,11 @@ begin
     -- RX Ack Gen
     -----------------------------------------------------------
 
-    tx_ack: process (rising_edge(i_clk), rising_edge(i_rst)) is
+    tx_ack: process (i_clk, i_rst) is
     begin
         if (i_rst) then
             o_rx_ack_sda <= '1';
-        else
+        elsif (rising_edge(i_clk)) then
             if (r_rx_currState = c_rx_wack) then
                 if (not i_bus_scl) then
                     o_rx_ack_sda <= (i_complete);
@@ -124,11 +129,11 @@ begin
         end if;
     end process;
 
-    byte_flag: process (rising_edge(i_clk), rising_edge(i_rst)) is
+    byte_flag: process (i_clk, i_rst) is
     begin
         if (i_rst) then
             o_byte_rdy <= '0';
-        else
+        elsif (rising_edge(i_clk)) then
             if (r_rx_currState = c_rx_wack) and (i_scl_falling_edge = '1') then
                 o_byte_rdy <= '1';
             else
